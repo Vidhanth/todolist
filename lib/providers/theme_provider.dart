5430621f7ef445f703161dd05d5c2d4b476d1da7
Vidhanth Bafna <vidhanthbafna@gmail.com>
2022-02-05 20:11:26 +0530

    Fix issue where no theme pref was saved

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todolist/utils/db_helper.dart';
 
 class ThemeProvider with ChangeNotifier {
-  bool _isDark = DBHelper.instance.getFromPrefs('is_dark');
+  bool _isDark = DBHelper.instance.getFromPrefs('is_dark') ?? false;
 
   bool get isDark => _isDark;
   set isDark(dark) {
