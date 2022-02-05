301e8ce72e4603fbc2fff6f29e78d8edfdb3db32
Vidhanth Bafna <vidhanthbafna@gmail.com>
2022-02-05 20:03:10 +0530

    Make themes persistent

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/utils/db_helper.dart';
 
 class ThemeProvider with ChangeNotifier {
-  bool _isDark = false;
+  bool _isDark = DBHelper.instance.getFromPrefs('is_dark');
 
   bool get isDark => _isDark;
   set isDark(dark) {
     _isDark = dark;
+    DBHelper.instance.saveToPrefs('is_dark', dark);
     notifyListeners();
   }
 }
